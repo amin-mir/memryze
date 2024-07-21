111e18c479c6e30293f2ace23b3cd9938745faaa
Amin Mir <aminmir326@gmail.com>
2024-07-21 14:30:56 +0300

    Implement serializing quiz

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,23 @@ use tracing::trace;
 
 pub mod db;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Message<'a> {
     Handshake { version: u8 },
+
     AddQA { q: &'a str, a: &'a str },
     AddQAResp,
+
+    GetQuiz,
+    Quiz { count: usize, qa_bytes: &'a [u8] },
+
     InternalError,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct QA<'a> {
+    q: &'a str,
+    a: &'a str,
 }
 
 // The reason I've created a struct to contain the input and output buffer
@@ -54,4 +65,56 @@ fn hex(data: &[u8]) -> String {
         hex.push_str(&format!("0x{:02X} ", byte));
     }
     hex
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quiz() {
+        let quiz = vec![
+            QA { q: "q1", a: "a1" },
+            QA { q: "q2", a: "a2" },
+            QA { q: "q3", a: "a3" },
+        ];
+
+        let mut msg_buf = vec![0u8; 512];
+        let mut qa_bytes = vec![0u8; 512];
+
+        // TODO: extract SERIALIZE to a function.
+        let mut buf_used = 0;
+        for i in 0..3 {
+            let used = postcard::to_slice(&quiz[i], &mut qa_bytes[buf_used..]).unwrap();
+            buf_used += used.len();
+        }
+
+        let msg = Message::Quiz {
+            count: 3,
+            qa_bytes: &qa_bytes[0..buf_used],
+        };
+
+        let used = postcard::to_slice(&msg, &mut msg_buf).unwrap();
+
+        // TODO: extract DESERIALIZE to a function.
+        let msg = postcard::from_bytes(used).unwrap();
+        let Message::Quiz {
+            count,
+            mut qa_bytes,
+        } = msg
+        else {
+            panic!("deserialized wrong type of Message: {:?}", msg);
+        };
+
+        assert_eq!(count, 3);
+        let mut res = Vec::with_capacity(count);
+        for _ in 0..count {
+            let (qa, unused) = postcard::take_from_bytes::<QA>(qa_bytes).unwrap();
+            res.push(qa);
+            qa_bytes = unused;
+        }
+
+        assert_eq!(res, quiz);
+        println!("{:?}", res);
+    }
 }
